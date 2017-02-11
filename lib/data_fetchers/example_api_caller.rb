9e74a75aee8439052145ee23730e64b934d19809
Siobhan <siobhan.harley@gmail.com>
2017-02-10 22:37:35 -0500

    updated changes with comments

--- lib/data_fetchers/example_api_caller.rb
+++ lib/data_fetchers/example_api_caller.rb
@@ -2,7 +2,9 @@ require "pry"
 class ExampleApi
 
   attr_reader :url, :artist_data, :artist_name
+  #Turned artist into a hash to accept the new k/v pairs of artist and popularity
  @@artists = {}
+
 
   def initialize(url)
     @url = url
@@ -13,15 +15,16 @@ class ExampleApi
     @@artists
   end
 
-
+  #created new hashes from artist/popularity keys
   def list_artists
     all_artists = artist_data["artists"]["items"]
-    all_artists.each_with_object({}) do | artist |
+    all_artists.each do | artist |
             artist_name = artist["name"]
             artists[artist_name] ||= {}
             artists[artist_name][:popularity] = artist["popularity"]
          end
     artists
   end
+
 
 end
--- lib/runners/example_cli.rb
+++ lib/runners/example_cli.rb
@@ -50,13 +50,15 @@ attr_accessor :input, :search, :artist_name, :artist_data
     search_term = input.split(" ").join("%20").downcase
     puts "Your search term was #{input.capitalize}, I am searching..."
     artist_data = ""
+
     counter = 0
+    ##This block runs the API request 5 times and incremets the offset by 50 to pull more results
     5.times do
     url = "http://api.spotify.com/v1/search?q=genre:#{search_term}&limit=50&offset=#{counter}&type=artist"
     artist_data = ExampleApi.new(url).list_artists
     counter += 50
-    puts "This worked!"
      end
+
     puts "Thank you for your patience. I found this on Spotify:"
     artist_data.each_with_index do |(artist, pop), index |
       puts " #{index + 1}. #{artist}"
@@ -66,16 +68,19 @@ attr_accessor :input, :search, :artist_name, :artist_data
 
   def popularity_filter
     popularity_input = input.to_i
+    #Input is a string, so has to be change to an integer
     puts "Returning all artists with a popularity of more than #{popularity_input}"
     artist_data.each do | artist, pop |
       pop.each do |k,v|
         if v < popularity_input
           @@pop_arr << artist
+          #pushing any artist with a popularity greater than input...can probably reconfinger this to accept a range
         end
       end
     end
   end
 
+ #broke this into a separate method
   def return_names
     puts "Here are the artists that meet your parameters:"
     @@pop_arr.each do | artist |
